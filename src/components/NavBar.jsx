7a436458f7bbe1399bd629e03faefd70e76b3a44
Carl <cfds2003@gmail.com>
2023-12-01 16:25:21 -0500

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,22 @@
 import Link from 'next/link'
+import { FaSearch } from "react-icons/fa";
+import { FaAngleDown } from "react-icons/fa6";
 
 export default function NavBar() {
-    const buttonStyle = 'px-5 py-1 uppercase border-2 border-[#000000] rounded-2xl'
+    const buttonStyle = 'font-bold px-5 py-1 uppercase border-2 border-[#000000] rounded-3xl'
   return (
     <nav className='bg-[#ffc700] text-black py-4 flex justify-around items-center'>
-        <div className='flex justify-between gap-4'>
+        <div className='flex justify-between gap-3'>
             <Link href="/" className={buttonStyle}>Shop</Link>
             <Link href="/about" className={buttonStyle}>About</Link>
-            <button>
-              <i class="fa-solid fa-magnifying-glass"></i>
+            <button className='ml-4'>
+              <FaSearch />
             </button>
         </div>
         <h1 className='uppercase font-bold text-2xl'>The store</h1>
-        <div className='flex justify-between gap-3'>
-            <button className={buttonStyle}>
-              United States <i class="fa-solid fa-chevron-down"></i>
+        <div className='flex justify-between gap-4'>
+            <button className={buttonStyle + ' flex items-center'}>
+              United States <FaAngleDown className='ml-10' />
             </button>
             <button className={buttonStyle}>Cart {0}</button>
         </div>
